9ce1e67419a2e2b135a6d3edc7887510fb39c030
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-09-28 17:21:49 -0300

    populate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+coin_award = AwardEntity.create(name: 'CollectedCoin')
+
+Rule.create([
+  { value: 1, award_entity: coin_award },
+  { value: 100, award_entity: coin_award },
+  { value: 1000, award_entity: coin_award },
+  { value: 10_000, award_entity: coin_award },
+  { value: 100_000, award_entity: coin_award }
+])
+
+death_award = AwardEntity.create(name: 'Death')
+
+Rule.create([
+   { value: 1, award_entity: death_award },
+   { value: 10, award_entity: death_award },
+   { value: 25, award_entity: death_award },
+   { value: 50, award_entity: death_award },
+   { value: 100, award_entity: death_award }
+ ])
+
+turtle = Monster.create(name: 'Turtle')
+bowser = Monster.create(name: 'Bowser')
+
+turtle_award = AwardEntity.create(name: turtle.name, entity: turtle)
+bowser_award = AwardEntity.create(name: bowser.name, entity: bowser)
+
+Rule.create([
+     { value: 1, award_entity: turtle_award },
+     { value: 100, award_entity: turtle_award },
+     { value: 1000, award_entity: turtle_award },
+     { value: 10_000, award_entity: turtle_award },
+     { value: 100_000, award_entity: turtle_award },
+
+     { value: 1, award_entity: bowser_award },
+     { value: 100, award_entity: bowser_award },
+     { value: 1000, award_entity: bowser_award },
+     { value: 10_000, award_entity: bowser_award },
+     { value: 100_000, award_entity: bowser_award }
+   ])
