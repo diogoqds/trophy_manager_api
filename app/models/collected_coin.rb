7eaf06d38f905b96f7f482ba212e6f675bba1b1a
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-09-28 16:26:57 -0300

    create collected_coin trophies

--- app/models/collected_coin.rb
+++ app/models/collected_coin.rb
@@ -6,4 +6,21 @@ class CollectedCoin < ApplicationRecord
 
   # validations
   validates :value, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  # callbacks
+  after_save :create_trophy
+
+  def create_trophy
+    ActiveRecord::Base.transaction do
+      award_entity = AwardEntity.find_by(name: model_name.name)
+      value = user.collected_coins.pluck(:value).sum
+      coin_rules = award_entity&.rules&.where(value: 1..value)
+      coin_rules&.each do |coin_rule|
+        Trophy.find_or_create_by!(
+          user: user,
+          rule: coin_rule
+        )
+      end
+    end
+  end
 end
--- spec/models/trophy_spec.rb
+++ spec/models/trophy_spec.rb
@@ -5,4 +5,22 @@ require 'rails_helper'
 RSpec.describe Trophy, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:rule) }
+
+  describe 'create coin trophies' do
+    let(:user) { create(:user) }
+    let(:collected_coin_award) { create(:award_entity, name: 'CollectedCoin') }
+
+    before do
+      create(:rule, value: 1, award_entity: collected_coin_award)
+      create(:rule, value: 10, award_entity: collected_coin_award)
+      create(:rule, value: 1000, award_entity: collected_coin_award)
+      create(:rule, value: 10_000, award_entity: collected_coin_award)
+      create(:rule, value: 100_000, award_entity: collected_coin_award)
+
+      create(:collected_coin, value: 100_000, user: user)
+      user.reload
+    end
+
+    it { expect(user.trophies.length).to eq(5) }
+  end
 end
