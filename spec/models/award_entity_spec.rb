e4b8aca4293b676b42ad9a1fdd8d3c8fa2c78777
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-09-30 14:44:00 -0300

    add check_name in award_entity

--- spec/models/award_entity_spec.rb
+++ spec/models/award_entity_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe AwardEntity, type: :model do
   it { is_expected.to validate_uniqueness_of(:name).ignoring_case_sensitivity }
   it { is_expected.to belong_to(:entity).optional }
   it { is_expected.to have_many(:rules).dependent(:destroy) }
+
+  describe '#check_name' do
+    let(:award_entity) { build(:award_entity, name: 'TableNotExist') }
+
+    it { expect(award_entity).not_to be_valid }
+  end
 end
