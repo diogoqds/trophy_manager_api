e40fb4519b00c61539daf3b877c64ba58ba4ba9b
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-10-02 15:26:07 -0300

    remove check_name validation

--- spec/models/award_entity_spec.rb
+++ spec/models/award_entity_spec.rb
@@ -7,19 +7,4 @@ RSpec.describe AwardEntity, type: :model do
   it { is_expected.to validate_uniqueness_of(:name).ignoring_case_sensitivity }
   it { is_expected.to belong_to(:entity).optional }
   it { is_expected.to have_many(:rules).dependent(:destroy) }
-
-  describe '#check_name' do
-    context 'when entity is nil' do
-      let(:award_entity) { build(:award_entity, name: 'TableNotExist') }
-
-      it { expect(award_entity).not_to be_valid }
-    end
-
-    context 'when entity is not nil' do
-      let(:monster) { create(:monster) }
-      let(:award_entity) { build(:award_entity, name: monster.name, entity: monster) }
-
-      it { expect(award_entity).to be_valid }
-    end
-  end
 end
